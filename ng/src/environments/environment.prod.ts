6204df8a4d90b17a23e2040f919c2a4f4b7af2ee
Ben Davidovich <davidovichben@gmail.com>
2025-11-02 15:53:38 +0200

    envs supabase change

--- ng/src/environments/environment.prod.ts
+++ ng/src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://lcazbaggfdejukjgkpeu.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxjYXpiYWdnZmRlanVramdrcGV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2OTE4MjgsImV4cCI6MjA2OTI2NzgyOH0.nRiCK9o830N-ZXvVALxTd2pLkDRQIZ8aCnnlo48IA5M',
-  aiEndpoint: 'https://hzukdzhhjpikmzgxrge.functions.supabase.co/ai',
   baseUrl: 'https://hoogi.com/',
   siteUrl: 'https://hoogi.com/',
   defaultLocale: 'he',
--- ng/src/environments/environment.staging.ts
+++ ng/src/environments/environment.staging.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'https://lcazbaggfdejukjgkpeu.supabase.co',
+  supabaseUrl: 'https://beokpwiubwfaaazyyukp.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxjYXpiYWdnZmRlanVramdrcGV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2OTE4MjgsImV4cCI6MjA2OTI2NzgyOH0.nRiCK9o830N-ZXvVALxTd2pLkDRQIZ8aCnnlo48IA5M',
-  aiEndpoint: 'https://hzukdzhhjpikmzgxrge.functions.supabase.co/ai',
   baseUrl: 'https://ai-4biz.netlify.app/',
   siteUrl: 'https://ai-4biz.netlify.app/',
   defaultLocale: 'he',
