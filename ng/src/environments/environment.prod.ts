675ae63cbc00d1977e0b421c9fecbbb61168d4c1
Ben Davidovich <davidovichben@gmail.com>
2025-11-02 16:57:45 +0200

    envs supabase change

--- ng/src/environments/environment.prod.ts
+++ ng/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://lcazbaggfdejukjgkpeu.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxjYXpiYWdnZmRlanVramdrcGV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2OTE4MjgsImV4cCI6MjA2OTI2NzgyOH0.nRiCK9o830N-ZXvVALxTd2pLkDRQIZ8aCnnlo48IA5M',
-  baseUrl: 'https://hoogi.com/',
-  siteUrl: 'https://hoogi.com/',
+  baseUrl: 'https://ihoogi.com/',
+  siteUrl: 'https://ihoogi.com/',
   defaultLocale: 'he',
   appName: 'Answer Tool',
   bypassProfileGuard: false
